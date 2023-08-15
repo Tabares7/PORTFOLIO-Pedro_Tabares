a721400c91022c27792687ca683e42cefb9e06f8
tabares7 <pedrotabares777@gmail.com>
2023-08-14 19:32:19 -0500

    Welcome component ready

--- portfolio/src/assets/components/PortfolioGrid.jsx
+++ portfolio/src/assets/components/PortfolioGrid.jsx
@@ -5,7 +5,7 @@ const PortfolioGrid = () => {
   return (
     <div className="lg:w-4/5 mx-auto w-full p-5">
       <div className="grid md:grid-cols-4 md:grid-rows-5 gap-3 min-h-[1300px]   grid-cols-2 grid-rows-8">
-        <div className="bg-red-100 md:col-span-3 md:row-span-2 overflow-hidden col-span-2 row-span-2 rounded-lg">
+        <div className="bg-red-100 md:col-span-3 md:row-span-2 overflow-hidden col-span-2 row-span-1 rounded-lg">
           <Welcome/>
         </div>
         <div className="bg-red-100 md:col-start-4 md:row-start-1 rounded-lg">
