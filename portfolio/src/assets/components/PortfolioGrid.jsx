1ad9c7a2720bf6e1a3fa437947a6eeb1dad02ce8
tabares7 <pedrotabares777@gmail.com>
2023-08-14 20:53:55 -0500

    Age Component Ready

--- portfolio/src/assets/components/PortfolioGrid.jsx
+++ portfolio/src/assets/components/PortfolioGrid.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Welcome from "./Welcome";
 import GitHub from "./GitHub";
 import Behance from "./Behance";
+import AgeComponent from "./AgeComponent";
 
 const PortfolioGrid = () => {
   return (
     <div className="lg:w-4/5 mx-auto w-full p-5">
       <div className="grid md:grid-cols-4 md:grid-rows-5 gap-3 min-h-[1300px]   grid-cols-2 grid-rows-8">
-        <div className="md:col-span-3 md:row-span-2 overflow-hidden col-span-2 row-span-1 rounded-lg">
+        <div className="md:col-span-3 md:row-span-2 overflow-hidden col-span-2 row-span-2 rounded-lg">
           <Welcome/>
         </div>
         <div className=" md:col-start-4 md:row-start-1 overflow-hidden rounded-lg">
@@ -16,10 +17,10 @@ const PortfolioGrid = () => {
         <div className=" md:col-start-4 md:row-start-2 overflow-hidden rounded-lg">
           <Behance/>
         </div>
-        <div className="bg-red-100 overflow-hidden rounded-lg">C4</div>
-        <div className="bg-red-100  overflow-hidden rounded-lg">C5</div>
+        <div className=" overflow-hidden rounded-lg"><AgeComponent/></div>
+        <div className="bg-red-100  overflow-hidden rounded-lg">Insta</div>
         <div className="bg-red-100 overflow-hidden row-start-4 col-span-2 md:col-span-2 rounded-lg">
-          C6
+          Projects
         </div>
         <div className="bg-red-100 overflow-hidden col-span-2 md:col-span-2 rounded-lg">C7</div>
         <div className="bg-red-100 overflow-hidden rounded-lg">C8</div>
