1f25faf580f4d2ff4fc0e78f740e0a8507d2c783
Pedro Tabares <99742692+Tabares7@users.noreply.github.com>
2023-08-15 15:48:28 -0500

    Adding Links

--- portfolio/src/assets/components/Behance.jsx
+++ portfolio/src/assets/components/Behance.jsx
@@ -3,12 +3,14 @@ import { BiLogoBehance, BiLinkExternal } from "react-icons/bi";
 
 const Behance = () => {
   return (
-    <div className="relative flex flex-col bg-trueorange text-palegray min-h-full items-center text-9xl justify-center mx-auto">
-      <div className="absolute top-5 right-5 text-gray-500 text-sm">
-        <BiLinkExternal />
+    <a href="https://www.behance.net/pedrotabares7" target="_blank">
+      <div className="relative flex flex-col bg-trueorange text-palegray min-h-full items-center text-9xl justify-center mx-auto">
+        <div className="absolute top-5 right-5 text-gray-500 text-sm">
+          <BiLinkExternal />
+        </div>
+        <BiLogoBehance />
       </div>
-      <BiLogoBehance />
-    </div>
+    </a>
   );
 };
 
--- portfolio/src/assets/components/GitHub.jsx
+++ portfolio/src/assets/components/GitHub.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { BiLogoGithub, BiLinkExternal } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const GitHub = () => {
   return (
-    <div className="relative flex flex-col bg-palegray text-trueorange min-h-full items-center text-9xl justify-center mx-auto">
-      <div className="absolute top-5 right-5 text-gray-500 text-sm">
-        <BiLinkExternal />
+    <a href="https://github.com/Tabares7" target="_blank">
+      <div className="relative flex flex-col bg-palegray text-trueorange min-h-full items-center text-9xl justify-center mx-auto">
+        <div className="absolute top-5 right-5 text-gray-500 text-sm">
+          <BiLinkExternal />
+        </div>
+        <BiLogoGithub />
       </div>
-      <BiLogoGithub />
-    </div>
+    </a>
   );
 };
 
