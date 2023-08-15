64603ac4c900b29918f99841a692a0852b1e6cd9
Pedro Tabares <99742692+Tabares7@users.noreply.github.com>
2023-08-15 15:29:02 -0500

    Mail Component ready so far

---
+++ portfolio/src/assets/components/Mail.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BiLogoGmail, BiLinkExternal } from "react-icons/bi";
+
+const Mail = () => {
+  return (
+    <div className="relative flex flex-col md:bg-lightgreen md:text-darkgrenn bg-darkgrenn text-lightgreen min-h-full items-center text-9xl justify-center mx-auto">
+      <div className="absolute top-5 right-5 text-gray-500 text-sm">
+        <BiLinkExternal />
+      </div>
+      <BiLogoGmail />
+    </div>
+  );
+};
+
+export default Mail;
--- portfolio/src/assets/components/PortfolioGrid.jsx
+++ portfolio/src/assets/components/PortfolioGrid.jsx
@@ -9,46 +9,47 @@ import ArtProjects from "./ArtProjects";
 import Lang from "./Lang";
 import Skills from "./Skills";
 import Bot from "./Bot";
+import Mail from "./Mail";
 
 const PortfolioGrid = () => {
   return (
     <div className="lg:w-4/5 mx-auto w-full p-5">
       <div className="grid md:grid-cols-4 md:grid-rows-5 gap-3 min-h-[1500px] md:min-h-[1300px]   grid-cols-2 grid-rows-8">
-        <div className="md:col-span-3  overflow-hidden col-span-2 row-span-2 rounded-lg">
+        <div className="md:col-span-3  overflow-hidden col-span-2 row-span-2 rounded-lg shadow-lg shadow-gray-400">
           <Welcome />
         </div>
-        <div className=" md:col-start-4 md:row-start-1 overflow-hidden rounded-lg">
+        <div className=" md:col-start-4 md:row-start-1 overflow-hidden rounded-lg shadow-lg shadow-gray-400">
           <GitHub />
         </div>
-        <div className=" md:col-start-4 md:row-start-2 overflow-hidden rounded-lg">
+        <div className=" md:col-start-4 md:row-start-2 overflow-hidden rounded-lg shadow-lg shadow-gray-400">
           <Behance />
         </div>
-        <div className=" overflow-hidden rounded-lg">
+        <div className=" overflow-hidden rounded-lg shadow-lg shadow-gray-400">
           <AgeComponent />
         </div>
-        <div className="overflow-hidden rounded-lg">
+        <div className="overflow-hidden rounded-lg shadow-lg shadow-gray-400">
           <Instagram />
         </div>
-        <div className="overflow-hidden row-start-4 md:row-start-3 md:col-start-3 col-span-2 rounded-lg">
+        <div className="overflow-hidden row-start-4 md:row-start-3 md:col-start-3 col-span-2 rounded-lg shadow-lg shadow-gray-400">
           <DevProjects />
         </div>
-        <div className=" overflow-hidden col-span-2 md:col-span-2 rounded-lg">
+        <div className=" overflow-hidden col-span-2 md:col-span-2 rounded-lg shadow-lg shadow-gray-400">
           <ArtProjects />
         </div>
-        <div className=" overflow-hidden rounded-lg">
+        <div className=" overflow-hidden rounded-lg shadow-lg shadow-gray-400">
           <Skills />
         </div>
-        <div className=" overflow-hidden rounded-lg">
+        <div className=" overflow-hidden rounded-lg shadow-lg shadow-gray-400">
           <Lang />
         </div>
-        <div className=" overflow-hidden rounded-lg">
+        <div className=" overflow-hidden rounded-lg shadow-lg shadow-gray-400">
           <Bot />
         </div>
-        <div className="bg-red-100 overflow-hidden col-span-2 row-start-8 rounded-lg">
+        <div className="bg-red-100 overflow-hidden col-span-2 row-start-8 rounded-lg shadow-lg shadow-gray-400">
           C11
         </div>
         <div className="bg-red-100 overflow-hidden col-start-2 row-start-7 md:row-start-5 md:col-start-4 rounded-lg">
-          C12
+          <Mail />
         </div>
       </div>
     </div>
