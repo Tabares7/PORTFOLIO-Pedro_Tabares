a3301c50a4e22aef76b6094083a2a68234be3936
Pedro Tabares <99742692+Tabares7@users.noreply.github.com>
2023-08-15 15:48:25 -0500

    Adding Links

--- portfolio/src/assets/components/Instagram.jsx
+++ portfolio/src/assets/components/Instagram.jsx
@@ -4,12 +4,14 @@ import { BiLogoInstagram, BiLinkExternal } from "react-icons/bi";
 
 const Instagram = () => {
   return (
-    <div className="relative flex flex-col bg-darkgrenn text-lightgreen min-h-full items-center text-9xl justify-center mx-auto">
-      <div className="absolute top-5 right-5 text-gray-500 text-sm">
-        <BiLinkExternal />
+    <a href="" target="_blank">
+      <div className="relative flex flex-col bg-darkgrenn text-lightgreen min-h-full items-center text-9xl justify-center mx-auto">
+        <div className="absolute top-5 right-5 text-gray-500 text-sm">
+          <BiLinkExternal />
+        </div>
+        <BiLogoInstagram />
       </div>
-      <BiLogoInstagram />
-    </div>
+    </a>
   );
 };
 
