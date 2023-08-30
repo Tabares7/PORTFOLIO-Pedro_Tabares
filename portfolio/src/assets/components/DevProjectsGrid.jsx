c49d5b0ef7c5d0749f455c376a36882fd1eb72d7
tabares7 <pedrotabares777@gmail.com>
2023-08-30 18:36:37 -0500

    Still

--- portfolio/src/assets/components/DevProjectsGrid.jsx
+++ portfolio/src/assets/components/DevProjectsGrid.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import PortfolioProject from "./projects/PortfolioProject";
 
 const DevProjectsGrid = () => {
   return (
     <div className="lg:w-4/5 mx-auto w-full p-5">
       <div className="grid grid-cols-2 grid-rows-8 gap-4 ">
         <div className="col-span-2 row-span-1 bg-gray-100 text-black min-h-[180px]">
-          Hola mundo
+          <PortfolioProject/>
         </div>
         <div className="col-span-2 row-span-1 bg-gray-100 text-black min-h-[180px]">
           Hola mundo
