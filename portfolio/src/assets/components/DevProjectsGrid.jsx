ce0b87d7f126d63666964b08d6003e48591660a2
tabares7 <pedrotabares777@gmail.com>
2023-08-30 18:56:54 -0500

    Project Portfolio Ready

--- portfolio/src/assets/components/DevProjectsGrid.jsx
+++ portfolio/src/assets/components/DevProjectsGrid.jsx
@@ -5,7 +5,7 @@ const DevProjectsGrid = () => {
   return (
     <div className="lg:w-4/5 mx-auto w-full p-5">
       <div className="grid grid-cols-2 grid-rows-8 gap-4 ">
-        <div className="col-span-2 row-span-1 bg-gray-100 text-black min-h-[180px]">
+        <div className="col-span-2 row-span-1 bg-gray-100 text-black min-h-[180px] rounded-lg shadow-lg shadow-gray-400 overflow-hidden md:min-h-[400px]">
           <PortfolioProject/>
         </div>
         <div className="col-span-2 row-span-1 bg-gray-100 text-black min-h-[180px]">
--- portfolio/src/assets/components/projects/PortfolioProject.jsx
+++ portfolio/src/assets/components/projects/PortfolioProject.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import portfolio from "../../img/portfolio-ss.png";
+import {BiRightArrowAlt} from "react-icons/bi"
+import { Link } from "react-router-dom";
 
 const PortfolioProject = () => {
   return (
+   <Link to="https://github.com/Tabares7/PORTFOLIO-Pedro_Tabares">
     <div
       className="h-full relative bg-center bg-cover cursor-pointer"
       style={{ backgroundImage: `url(${portfolio})` }}
     >
-        <div className="absolute z-10 bg-gradient-to-t from-darkgrenn via-green-800 to-transparent w-full h-full opacity-80">        
+        <div className="duration-150 absolute z-10 bg-gradient-to-t from-darkgrenn via-green-900 to-transparent w-full h-full opacity-80 hover:opacity-95">        
         </div>
-        <h2 className="absolute text-white bottom-4 left-4 z-20 font-bold tracking-wide"> My own portfolio</h2>
+        <div className="absolute text-white bottom-2 left-2 z-20 p-2 md:bottom-5 md:left-5 md:p-4">
+        <h2 className=" font-bold tracking-wide md:text-2xl">🎓 My own Portfolio</h2>
+        <p className="text-sm md:text-lg">Portfolio built with React and JavaScript, styled with Tailwind. Check it out!</p>
+        </div>
+        <div className="absolute top-5 right-5  text-white md:text-2xl text-lg"><BiRightArrowAlt/></div>
     </div>
+   </Link>
   );
 };
 
