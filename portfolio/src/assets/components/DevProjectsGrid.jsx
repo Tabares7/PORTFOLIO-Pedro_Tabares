0ff5ad287f6452291dc9d7f09539d95f95d0e68b
tabares7 <pedrotabares777@gmail.com>
2023-08-30 17:32:17 -0500

    Still working

--- portfolio/src/assets/components/DevProjectsGrid.jsx
+++ portfolio/src/assets/components/DevProjectsGrid.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const DevProjectsGrid = () => {
   return (
-    <div className='grid grid-cols-2 grid-rows-8'>
-        
+    <div className="lg:w-4/5 mx-auto w-full p-5">
+      <div className="grid grid-cols-2 grid-rows-8 gap-4 ">
+        <div className="col-span-2 row-span-1 bg-gray-100 text-black min-h-[180px]">
+          Hola mundo
+        </div>
+        <div className="col-span-2 row-span-1 bg-gray-100 text-black min-h-[180px]">
+          Hola mundo
+        </div>
+        <div className="col-span-1 row-span-2 bg-gray-100 text-black min-h-[360px] ">
+          Hola mundo
+        </div>
+        <div className="col-span-1 row-span-2 bg-gray-100 text-black min-h-[360px] ">
+          Hola mundo
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DevProjectsGrid
+export default DevProjectsGrid;
