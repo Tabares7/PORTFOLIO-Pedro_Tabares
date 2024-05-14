46df274c1ed4e3035b634147aa4ba1a64ad7cba4
labuenafe <labuenafe21@gmail.com>
2024-05-14 18:43:21 -0500

    Refactor project descriptions in DevProjectsGrid and ProjectTemplate components

--- portfolio/src/assets/components/DevProjectsGrid.jsx
+++ portfolio/src/assets/components/DevProjectsGrid.jsx
@@ -11,7 +11,7 @@ const DevProjectsGrid = () => {
       <ProjectTemplate
       logo={assess101}
       title="Assess101"
-      description="I developed an innovative fullstack application designed for primary care clinics, aimed at centralizing and optimizing the medical evaluation process. This tool consolidates a wide variety of screening forms into one place, allowing healthcare providers to generate comprehensive evaluation sessions and apply multiple questionnaires efficiently."
+      description="An innovative fullstack application designed for primary care clinics, aimed at centralizing and optimizing the medical evaluation process. This tool consolidates a wide variety of screening forms into one place, allowing healthcare providers to generate comprehensive evaluation sessions and apply multiple questionnaires efficiently."
       screenshot={assess101IMG}
       aiText="AI powered"
       patientInfo={{ month: '8', today: '8' }}
